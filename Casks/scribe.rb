c631c7332c8a8b3a0e30b0c2428acbcbe71a3589
Nuno Mendes <nuno.men@outlook.com>
2026-01-29 19:32:48 +0100

    Update scribe to v0.0.6 (actual latest release)

--- Casks/scribe.rb
+++ Casks/scribe.rb
@@ -1,6 +1,6 @@
 cask "scribe" do
-  version "1.0.0"
-  sha256 :no_check
+  version "0.0.6"
+  sha256 "f94002f4fbce4150595e0a8c760ab47770ffbdab8ea49b046d96ddb6beb49796"
 
   url "https://github.com/usescrolls/scribe/releases/download/v#{version}/scribe-Installer.dmg",
       verified: "github.com/usescrolls/scribe/"
